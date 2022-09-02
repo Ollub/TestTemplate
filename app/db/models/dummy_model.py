276741cbbcbeee178caf4a49065151732211926b
Oleg <orlovoa@pik.ru>
2022-09-02 11:38:55 -0600

    initial

---
+++ app/db/models/dummy_model.py
@@ -0,0 +1,12 @@
+from app.db.base import Base
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import Integer, String
+
+
+class DummyModel(Base):
+    """Model for demo purpose."""
+
+    __tablename__ = "dummy_model"
+
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    name = Column(String(length=200))  # noqa: WPS432
